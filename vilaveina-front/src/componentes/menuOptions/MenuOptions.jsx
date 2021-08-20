a7010bd932ecede8990fefb07d52735ccb4ec547
pamela salazar <you@example.com>
2021-08-20 12:54:54 +0200

    Iniciando a hacer cambios css

--- vilaveina-front/src/componentes/menuOptions/MenuOptions.jsx
+++ vilaveina-front/src/componentes/menuOptions/MenuOptions.jsx
@@ -3,6 +3,8 @@ import imgLogoVeina from "../img/LogoVila.png";
 import logoAgenda from "../img/LogoAgenda.jpg";
 import logoAjuntament from "../img/LogoAjuntament.gif";
 import logoFoment from "../img/LogoFoment.png";
+import listaVerde from "../img/listaVerde.png"
+import estadisticaNaranja from "../img/estadisticaNaranja.png"
 import "./menuOptions.css";
 
 
@@ -12,20 +14,21 @@ export const MenuOptions = () => (
         <div className="LogoVilaMenuPage">
             <img src={imgLogoVeina} alt="logo veina" className="LogoVilaMenuPage"/>
         </div>
+
         <div className="buttonsmenu">
-        <button className="leftbutton">
-            <img src={imgLogoVeina} alt="logo veina" className="iconoEncuesta"/>
+        <button className="button">
+            <img src={listaVerde} alt="logo veina" className="iconoEncuesta"/>
         </button>
             <h1 className="titulosBotones"> Encuesta </h1>
-        <button className="rightbutton">
-            <img src={imgLogoVeina} alt="logo veina" className="iconoEstadistica"/>
+        <button className="button">
+            <img src={estadisticaNaranja} alt="logo veina" className="iconoEncuesta"/>
         </button>
             <h1 className="titulosBotones"> Estadisticas </h1>
 
         </div>
 
+        <div className="line"> </div>
         <footer>
-            <div className="line"> </div>
             <img src={logoAgenda} alt="logo agenda"/>
             <img src={logoAjuntament} alt="logo ajuntament"/>
             <img src={logoFoment} alt="logo foment"/>
