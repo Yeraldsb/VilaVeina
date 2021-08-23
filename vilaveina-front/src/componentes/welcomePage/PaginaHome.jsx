cead66c32cba53cd51895235f82ad29a38c40f32
Gaby8909 <gabi_oa@hotmail.com>
2021-08-23 14:50:13 +0200

    diseño pagina home

--- vilaveina-front/src/componentes/welcomePage/PaginaHome.jsx
+++ vilaveina-front/src/componentes/welcomePage/PaginaHome.jsx
@@ -5,30 +5,31 @@ import logoAjuntament from "../img/LogoAjuntament.gif";
 import logoFoment from "../img/LogoFoment.png";
 import PopupModal from "../popUp/PopupModal";
 import {useState} from "react";
+import "./paginahome.css"
 
 
 export function PaginaHome() {
     const [buttonPopup, setButtonPopup] = useState(false);
 
     return (
-        <div>
+        <div className="containerHome">
             <div className="containerLogoVila">
                 <img src={imgLogoVeina} alt="logo veina"/>
             </div>
 
             <button className="containerEntrar" onClick={() => setButtonPopup(true)}> Entrar</button>
+            <div className="containerFooter">
+                <img className="imgAgenda" src={logoAgenda} alt="logo agenda"/>
+                <img className="imgAyuntament" src={logoAjuntament} alt="logo ajuntament"/>
+                <img className="imgFoment" src={logoFoment} alt="logo foment"/>
 
+            </div>
             <PopupModal trigger={buttonPopup} setTrigger={setButtonPopup}>
 
             </PopupModal>
 
 
-            <div> footer
-                <img src={logoAgenda} alt="logo agenda"/>
-                <img src={logoAjuntament} alt="logo ajuntament"/>
-                <img src={logoFoment} alt="logo foment"/>
 
-            </div>
 
         </div>
     )
