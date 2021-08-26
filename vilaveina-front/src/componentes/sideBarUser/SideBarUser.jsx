09a254158bc2c97b470d42c72d9bd8b579a547c8
pamela salazar <you@example.com>
2021-08-26 14:55:22 +0200

    Yeraldin: sidebar Done

--- vilaveina-front/src/componentes/sideBarUser/SideBarUser.jsx
+++ vilaveina-front/src/componentes/sideBarUser/SideBarUser.jsx
@@ -8,12 +8,13 @@ import {Footer} from "../footer/Footer";
 export default function SideBarUser() {
     return (
         <div className="sidebar">
-                <img src={vila} alt="" className="vila"/>
-                <div className="containerButton">
-                    <button><img src={green} alt="" className="home"/>Home</button>
-                    <br/>
-                    <button><img src={orange} alt="" className="home"/>About</button>
-                </div>
+            <img src={vila} alt="" className="vila"/>
+            <div className="containerButton">
+                <button><img src={green} alt="" className="logoSidebar"/>Nova enquesta</button>
+                <br/>
+                <button><img src={orange} alt="" className="logoSidebar"/>Estadístiques</button>
+            </div>
+            <Footer/>
         </div>
     )
 }
