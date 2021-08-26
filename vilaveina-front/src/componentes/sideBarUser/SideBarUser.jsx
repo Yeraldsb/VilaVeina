c5bce6f42abfdb2d840a24f5a00e646c91fea43f
Sonia Mestre <mestre.sonia@gmail.com>
2021-08-26 11:23:20 +0200

    sidebar base design

--- vilaveina-front/src/componentes/sideBarUser/SideBarUser.jsx
+++ vilaveina-front/src/componentes/sideBarUser/SideBarUser.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import "./sideBarUser.css"
 
 export default function SideBarUser(){
     return(
-        <div className="sideBar">
-            <div className="sideBarWrapper">
-                <div className="sideBarMenu">
-                    <ul className="sideBarList">
-
-                    </ul>
-                </div>
-            </div>
-
+        <div className="sidebar">
+            <h1>Brand</h1>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Portfolio</a>
+            <a href="">Gallery</a>
+            <a href="">Service</a>
+            <a href="">Join</a>
+            <a href="">Contact</a>
         </div>
     )
 }
