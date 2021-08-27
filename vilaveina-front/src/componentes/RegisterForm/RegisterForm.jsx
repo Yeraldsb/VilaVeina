38896a5f2f98022af21bf900a90686e02b5d38d3
Nicole Uy <marvienicolebtw@hotmail.com>
2021-08-27 15:07:09 +0200

    register form style

--- vilaveina-front/src/componentes/RegisterForm/RegisterForm.jsx
+++ vilaveina-front/src/componentes/RegisterForm/RegisterForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./RegisterForm.css";
+import Form from "react-bootstrap/Form";
 
 
 
@@ -7,26 +8,57 @@ import "./RegisterForm.css";
 export const RegisterForm = () => {
 
     return (
-        <div className={"regitroFormdiv"}>
-            <div className={"nameImput"}>
-                <label>Nom</label>
-                <input type="text"  className="registName"  required />
-                <label>Cognom</label>
-                <input type="text"  className="registCognom"  required />
-            </div>
-            <label>Adreça</label>
-            <input type="text"  className="registAdreça"  required />
+        <div className={"registerForm"}>
+            <div className={"columnpile"}>
 
-    <div className={"nameCorreu"}>
-        <label>Correu Electrònic</label>
-        <input type="text"  className="registCorreu"  required />
-        <label>Telèfon</label>
-        <input type="text"  className="registTel"  required />
-    </div>
-            <button className={"submitRegForm"} type="submit" >Guardar</button>
+                <div className={"columna1"}>
+                    <label>Nom</label>
+                    <input type="text"  className="register-name"  required />
+                    <label>Adreça</label>
+                    <input type="text"  className="register-address"  required />
+                    <label>Correu Electrònic</label>
+                    <input type="text"  className="register-email"  required />
+                    <label>Usuari</label>
+                    <input type="text"  className="register-email"  required />
+                </div>
+
+                <div className={"columna2"}>
+                    <label>Cognom</label>
+                    <input type="text"  className="register-surname"  required />
+                    <Form.Group>
+                        <Form.Label> Vila Veïna : </Form.Label>
+                        <Form.Select>
+                            <option></option>
+                            <option>Badal</option>
+                            <option>Carmel de dalt</option>
+                            <option>Casc antic d'Horta</option>
+                            <option>Casc antic Les Corts</option>
+                            <option>Consell de Cent - Girona - Eixample</option>
+                            <option>El Camp de l'Arpa - Alchemika</option>
+                            <option>El Congrés i els Indians</option>
+                            <option>Gotic</option>
+                            <option>La Marina - Mare de Déu del Port</option>
+                            <option>La Trinidad Vella</option>
+                            <option>Maresme</option>
+                            <option>Prosperitat</option>
+                            <option>Provençals de Poblenou</option>
+                            <option>Sant Gervasi de Cassoles</option>
+                            <option>Vila de Gracia</option>
+                            <option>Vilapicina i la Torre LLobeta - Cotxeres</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <label>Telèfon</label>
+                    <input type="text"  className="register-phone"  required />
+                    <label>Contrasenya</label>
+                    <input type="password"  className="register-phone"  required />
+                </div>
+            </div>
+
+            <button className={"submitRegForm"} type="submit" > Guardar </button>
 
         </div>
 
 
     )
 }
+
