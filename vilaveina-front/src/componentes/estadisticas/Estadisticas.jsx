51e5c28ead81668e6776186db997b238f87657e8
Sonia Mestre <mestre.sonia@gmail.com>
2021-08-30 11:22:38 +0200

    modificación botones borde gris componente estadistiques

--- vilaveina-front/src/componentes/estadisticas/Estadisticas.jsx
+++ vilaveina-front/src/componentes/estadisticas/Estadisticas.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import "./estadisticas.css"
 import green from '../img/listaVerde.png'
 import orange from '../img/estadisticaNaranja.png'
-
+import CustomDataPicker from "../Form/CustomDatePicker";
 
 export default function Estadisticas() {
     return (
         <div className="containerestadisticas">
+        <CustomDataPicker />
             <div className="containerbotones">
                 <button className="botones"><img src={green} alt="" className="botonfiltro"/>Gènere</button>
-                <br/>
                 <button className="botones"><img src={green} alt="" className="botonfiltro"/>Edat</button>
-                <br/>
                 <button className="botones"><img src={orange} alt="" className="botonfiltro"/>Lloc naixement</button>
                 <button className="botones"><img src={orange} alt="" className="botonfiltro"/>Enquestes realitzades</button>
                 <button className="botones"><img src={orange} alt="" className="botonfiltro"/>Consulta a títol</button>
                 <button className="botones"><img src={orange} alt="" className="botonfiltro"/>Cures /suport</button>
+            </div>
+            <div className="containergrafica">
+                <p>aquí irá la gráfica</p>
             </div>
         </div>
 
