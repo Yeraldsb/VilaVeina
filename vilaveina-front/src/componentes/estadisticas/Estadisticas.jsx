df7ba4b506d313e17d0440c6c24945055119c2d2
superco-art <daniela.trifu@gmail.com>
2021-08-30 14:07:38 +0200

    calendar stadistics

--- vilaveina-front/src/componentes/estadisticas/Estadisticas.jsx
+++ vilaveina-front/src/componentes/estadisticas/Estadisticas.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./estadisticas.css"
 import edat from '../img/altaBaja.png'
 import naixement from '../img/arbre.png'
@@ -6,17 +6,27 @@ import enquestes from '../img/xarxa.png'
 import consulta from '../img/escales.png'
 import suport from '../img/cor.png'
 import genere from '../img/cures.png'
-import CustomDataPicker from "../Form/CustomDatePicker";
+import StadisticsCalendar from "./StadisticsCalendar";
+
 
 
 export default function Estadisticas() {
+     const [datepick, setDatePick] = useState(new Date());
+
     return (
         <div className="bodyestadisticas">
             <div className="containerestadisticas">
-                <div className="datapicker-estadisticas">
-                    <CustomDataPicker />
-                </div>
+
                     <div className="containerbotones">
+                        <div className="datapicker-estadisticas">
+                            <StadisticsCalendar
+                                className="form-control"
+                                dateFormat="dd/MM/yyyy"
+                                selected={datepick}
+                                onChange={(date) => setDatePick(date)}
+                            />
+
+                        </div>
                         <button className="botones" ><img src={genere} alt="" className="iconoboton"/>Gènere</button>
                         <button className="botones"><img src={edat} alt="" className="iconoboton"/>Edat</button>
                         <button className="botones"><img src={naixement} alt="" className="iconoboton"/>Lloc naixement</button>
